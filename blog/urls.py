451604b985db27deaed9faa8b3fddbe71bc2b71b
henzpp <lynn5863@hotmail.com>
2022-08-10 18:32:27 +0900

    기깔나는 랜딩페이지 성공

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.PostList.as_view()),
+    path('tag/<str:slug>/', views.tag_page),
+    path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
-    #path('<int:pk>/', views.single_pages),
+    path('',views.PostList.as_view()),
+    #path('',views.index),
 ]
+
